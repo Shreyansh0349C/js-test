29321422a72492be4f2fba9534bbdd4a61271d2f
Shreyansh Katiyar <shreyanshkatiyar885@gmail.com>
2024-09-21 19:24:53 +0530

    Comparison

---
+++ devcontainer/01_file/comparison.js
@@ -0,0 +1,7 @@
+console.log("2">1);
+console.log("02">1);
+//console.log(null>0);
+//console.log(null>=0);
+//console.log(null==0);  don't use this type of comparison 
+console.log("2"===2)
+
--- devcontainer/01_file/conversionoperations.js
+++ devcontainer/01_file/conversionoperations.js
@@ -1,8 +1,28 @@
-let score="shreyansh"
-console.log(typeof score)
-// let score1=10
-let isNumber=Number(score)
-console.log(isNumber,typeof isNumber)
-let score3=true
-let isNumber2=Number(score3)
-console.log(isNumber2,typeof isNumber2)
+// let score="shreyansh"
+// console.log(typeof score)
+// // let score1=10
+// let isNumber=Number(score)
+// console.log(isNumber,typeof isNumber)
+// let score3=true
+// let isNumber2=Number(score3)
+// console.log(isNumber2,typeof isNumber2)
+// /* prefix and postfix8*/
+// let isVariable=101
+// ++isVariable, isVariable++;
+// console.log(isVariable)
+
+
+let x = 3;
+const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+//console.log(`a:${a}, b:${b}`)
+// Expected output: "a:4, b:4"
+console.log("a=",a)
+console.log(`a=${a}`)
