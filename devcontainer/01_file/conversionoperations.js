8556d66f0ec04ddd83d97e05bce077701e8a15f1
Shreyansh Katiyar <shreyanshkatiyar885@gmail.com>
2024-09-21 18:15:17 +0530

    Conversion Operations

---
+++ devcontainer/01_file/conversionoperations.js
@@ -0,0 +1,8 @@
+let score="shreyansh"
+console.log(typeof score)
+// let score1=10
+let isNumber=Number(score)
+console.log(isNumber,typeof isNumber)
+let score3=true
+let isNumber2=Number(score3)
+console.log(isNumber2,typeof isNumber2)
