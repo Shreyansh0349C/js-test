43d12a863c3c302acec997c65e7a15e709b35bce
Shreyansh Katiyar <shreyanshkatiyar885@gmail.com>
2024-09-21 22:39:27 +0530

    Datatypes Summary

--- devcontainer/01_file/comparison.js
+++ devcontainer/01_file/comparison.js
@@ -4,4 +4,3 @@ console.log("02">1);
 //console.log(null>=0);
 //console.log(null==0);  don't use this type of comparison 
 console.log("2"===2)
-
--- devcontainer/01_file/datatypes.js
+++ devcontainer/01_file/datatypes.js
@@ -7,3 +7,36 @@ null
 undefined
 symbol==unique
 object*/
+
+// Primitive
+ // String,Number,Boolean,null,undefined,Symbol,BigInt
+
+const score=100
+const scorevalue=100.3
+
+const isLoggedin=false
+const outsideTem=null
+let userEmail;
+
+const Id=Symbol('123')
+const anotherID=Symbol('123')
+
+console.log(Id===anotherID);
+
+// const Bignumber=534132164135416321613416841315n
+
+// Refrence (Non-primitive)
+
+// Array,Objects,Functions
+
+const heroes=["shaktiman","naagraj","doga"]
+
+let myObj={
+    name:"shreyansh",
+    age:22
+}
+
+const myFunction=function(){
+    console.log("Hello World");
+}
+
