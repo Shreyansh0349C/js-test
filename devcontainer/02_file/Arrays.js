54702dcf0788e82df2d6909256ea941743a16fe7
Shreyansh Katiyar <shreyanshkatiyar885@gmail.com>
2024-09-23 12:31:35 +0530

    Arrays1

---
+++ devcontainer/02_file/Arrays.js
@@ -0,0 +1,36 @@
+// array
+
+const myArr=[0,1,2,3,4,5]
+const myHeroes=["batman","superman","ironman"]
+
+const myArr2= new Array(1,2,3,4,5,6)
+//console.log(myArr[4]);
+
+//Array Methods
+
+// myArr.push(6)
+// myArr.shift()
+// myArr.pop()
+// myArr.unshift(9)
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr=myArr.join()
+
+// comsole.log(myArr);
+// console.log(newArr);
+
+// Slice,Splice
+
+console.log("A",myArr);
+
+const myNew1=myArr.slice(1,3)
+
+console.log(myNew1);
+console.log("B",myArr);
+
+const myNew2=myArr.splice(1,3)
+console.log("c",myArr);
+console.log(myNew2);
+
